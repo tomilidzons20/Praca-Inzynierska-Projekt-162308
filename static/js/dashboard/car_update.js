5dffcdd484e0a802418e7c08428ea94fbef27a3d
Jakub Pietkiewicz <jakubpietkiewicz@outlook.com>
2023-11-23 16:52:35 +0100

    Add datatables Add datatable to car update view

---
+++ static/js/dashboard/car_update.js
@@ -0,0 +1,47 @@
+const createDataTable = (id) => {
+  const sumColumnNumber = 2;
+  let table = new DataTable(id, {
+    "autoWidth": true,
+    responsive: {
+      details: {
+        display: $.fn.dataTable.Responsive.display.modal({
+          header: (row) => {
+            const data = row.data();
+            const rowIndex = row.index() + 1
+            return 'Details for ' + rowIndex + ''
+          }
+        }),
+        renderer: $.fn.dataTable.Responsive.renderer.tableAll(),
+      }
+    },
+    "info": false,
+    footerCallback: function (row, data, start, end, display) {
+      let api = this.api();
+      // Remove the formatting to get integer data for summation
+      let Val = function (i) {
+        return typeof i === 'string'
+          ? i.replace(/[\ PLN]/g, '') * 1
+          : typeof i === 'number'
+            ? i
+            : 0;
+      };
+      // Total over all pages
+      total = api
+        .column(sumColumnNumber)
+        .data()
+        .reduce((a, b) => Val(a) + Val(b), 0);
+      // Total over this page
+      pageTotal = api
+        .column(sumColumnNumber, { page: 'current' })
+        .data()
+        .reduce((a, b) => Val(a) + Val(b), 0);
+      // Update footer
+      api.column(sumColumnNumber).footer().innerHTML =
+        pageTotal + ' PLN' + ' (' + total + ' PLN)';
+    },
+  });
+}
+
+$(document).ready(() => {
+  createDataTable('#maintenance-table');
+})
